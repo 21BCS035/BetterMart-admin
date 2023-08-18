a979ea0e297946e172a3bee43f7fb380cd52c3a0
Arpit <ar5136874yadav@gmail.com>
2023-08-18 19:21:57 +0530

    changes

---
+++ src/features/auth/authService.js
@@ -0,0 +1,31 @@
+import axios from "axios"
+import { base_url } from "../../utils/base_url"
+import {config} from "../../utils/axiosconfig"
+
+const login = async(user)=>{
+    const response = await axios.post(`${base_url}user/admin-login`,user);
+    if(response.data)
+    {
+        localStorage.setItem("user",JSON.stringify(response.data))
+    }
+
+    return response.data;
+};
+
+const getOrders = async () => {
+    const response = await axios.get(`${base_url}user/getallOrders`,config);
+  
+    return response.data;
+  };
+
+  const deleteOrder = async (id) => {
+    const response = await axios.delete(`${base_url}user/deletebrand/${id}`, config);
+    return response.data;
+  };
+
+const authService = {
+    login,
+    getOrders,
+}
+
+export default authService;
